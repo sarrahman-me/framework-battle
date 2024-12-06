d15dee210e5182fee67efb5cdd9bd91db419bbfe
muhammad nur rahman <138513675+sarrahman-me@users.noreply.github.com>
2024-12-06 07:24:00 +0700

    :hammer: membuat untuk rust

--- actix-rust/src/main.rs
+++ actix-rust/src/main.rs
@@ -1,16 +1,17 @@
-use actix_web::{
-    delete, get, middleware::Logger, patch, post, web, App, HttpResponse, HttpServer, Responder,
-};
-use env_logger::Env;
-use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
 
+use actix_web::{
+    delete, get, middleware::Logger, patch, post, web, App, HttpResponse, HttpServer, Responder,
+};
+use env_logger::Env;
+use serde::{Deserialize, Serialize};
+
 type DB = Arc<Mutex<HashMap<u32, Book>>>;
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Book {
     id: Option<u32>,
     title: String,
@@ -18,18 +19,25 @@ struct Book {
     year: i32,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct ResponseType<T> {
     message: String,
     data: Option<T>,
 }
 
 #[post("/books")]
-async fn add_products(payload: web::Json<Book>, db: web::Data<DB>) -> impl Responder {
+async fn add_new_book(payload: web::Json<Book>, db: web::Data<DB>) -> impl Responder {
     let mut db = db.lock().unwrap();
     let payload = payload.into_inner();
 
     let new_id: u32 = db.keys().max().unwrap_or(&0) + 1;
+
+    let new_book = Book {
+        id: Some(new_id),
+        title: payload.title.to_owned(),
+        author: payload.author.to_owned(),
+        year: payload.year,
+    };
 
     if db.values().any(|item| item.title == payload.title.clone()) {
         return HttpResponse::Conflict().json(ResponseType {
@@ -38,23 +46,16 @@ async fn add_products(payload: web::Json<Book>, db: web::Data<DB>) -> impl Respo
         });
     }
 
-    let new_product = Book {
-        id: Some(new_id),
-        title: payload.title,
-        author: payload.author,
-        year: payload.year,
-    };
-
-    db.insert(new_id, new_product.clone());
+    db.insert(new_id, new_book.clone());
 
     HttpResponse::Ok().json(ResponseType {
-        message: "Berhasil menambahkan data buku".to_string(),
-        data: Some(new_product),
+        message: "Berhasil menambahkan data buku baru".to_string(),
+        data: Some(new_book.clone()),
     })
 }
 
 #[get("/books")]
-async fn find_all_products(db: web::Data<DB>) -> impl Responder {
+async fn find_all_books(db: web::Data<DB>) -> impl Responder {
     let db = db.lock().unwrap();
 
     let all_books: Vec<Book> = db.values().cloned().collect();
@@ -66,46 +67,50 @@ async fn find_all_products(db: web::Data<DB>) -> impl Responder {
 }
 
 #[patch("/books/{id}")]
-async fn update(id: web::Path<u32>, payload: web::Json<Book>, db: web::Data<DB>) -> impl Responder {
+async fn update_book(
+    id: web::Path<u32>,
+    payload: web::Json<Book>,
+    db: web::Data<DB>,
+) -> impl Responder {
     let mut db = db.lock().unwrap();
     let id = id.into_inner();
     let payload = payload.into_inner();
 
     if db.get(&id).is_none() {
         return HttpResponse::NotFound().json(ResponseType {
-            message: "Data buku tidak ditumukan".to_string(),
+            message: "Buku tidak ditemukan".to_string(),
             data: None::<Book>,
         });
-    };
+    }
 
-    let product_to_update = Book {
+    let book_to_update = Book {
         id: Some(id),
-        title: payload.title.clone(),
+        title: payload.title.to_owned(),
         author: payload.author.to_owned(),
         year: payload.year,
     };
 
-    db.insert(id, product_to_update);
+    db.insert(id, book_to_update);
 
-    let updated_product = db.get(&id).unwrap();
+    let updated_book = db.get(&id).unwrap();
 
     HttpResponse::Ok().json(ResponseType {
         message: "Berhasil mengupdate data buku".to_string(),
-        data: Some(updated_product),
+        data: Some(updated_book),
     })
 }
 
 #[delete("/books/{id}")]
-async fn delete(id: web::Path<u32>, db: web::Data<DB>) -> impl Responder {
+async fn delete_book(id: web::Path<u32>, db: web::Data<DB>) -> impl Responder {
     let mut db = db.lock().unwrap();
     let id = id.into_inner();
 
     match db.remove(&id) {
-        Some(product) => HttpResponse::Ok().json(ResponseType {
+        Some(book) => HttpResponse::Ok().json(ResponseType {
             message: "Berhasil menghapus data buku".to_string(),
-            data: Some(product),
+            data: Some(book),
         }),
-        None => HttpResponse::BadRequest().json(ResponseType {
+        None => HttpResponse::NotFound().json(ResponseType {
             message: "Data buku tidak ditemukan".to_string(),
             data: None::<Book>,
         }),
@@ -113,10 +118,7 @@ async fn delete(id: web::Path<u32>, db: web::Data<DB>) -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let port = 8080;
-    println!("Server running on port {}", port);
-
+async fn main() -> Result<(), std::io::Error> {
     let app_data: DB = Arc::new(Mutex::new(HashMap::<u32, Book>::new()));
 
     env_logger::init_from_env(Env::default().default_filter_or("debug"));
@@ -125,12 +127,12 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .app_data(web::Data::new(app_data.clone()))
-            .service(add_products)
-            .service(find_all_products)
-            .service(update)
-            .service(delete)
+            .service(add_new_book)
+            .service(find_all_books)
+            .service(update_book)
+            .service(delete_book)
     })
-    .bind(("0.0.0.0", port))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
